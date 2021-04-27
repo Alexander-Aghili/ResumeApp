56375001e97933674c57f9a86176ba420ad1a780
Alexander Aghili <43054019+Alexander-Aghili@users.noreply.github.com>
2021-04-26 20:17:04 -0700

    Update Driver.java

--- src/main/java/com/rest/resume/Driver.java
+++ src/main/java/com/rest/resume/Driver.java
@@ -1,56 +1,49 @@
-package com.rest.resume;
-
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
+import com.rest.resume.ResumeS3;
+
 public class Driver {
-    private String input;
-    private final Scanner scan = new Scanner(System.in);
-    private ResumeS3 resume;
-    private final String httpPath = "http://localhost:8081/ResumeApp/Resume/list";
+    private static final Scanner scan = new Scanner(System.in);
+    private static ResumeS3 resume;
+    private static final String httpPath = "http://localhost:8081/ResumeApp/Resume/list";
     
-    public void main(String[] args) {
+    public static void main(String[] args) {
     	resume = new ResumeS3();
     	
         System.out.println();
         System.out.println("--- Main Menu ---");
-        System.out.println("Would you like to upload a file or download a file? (Enter \"up\" or \"down\") You can also list all files by typing \"print\". Type \"stop\" to stop the program.");
-        input = scan.nextLine();
-
-        while(!input.equalsIgnoreCase("up") && !input.equalsIgnoreCase("down") && !input.equalsIgnoreCase("end") && !input.equalsIgnoreCase("list")) {
-            System.out.println("Invalid input. Enter \"up\", \"down\", or \"list\".");
-            input = scan.nextLine();
+        
+        while(true) {
+        	System.out.println("Would you like to upload a file or download a file? (Enter \"up\" or \"down\") You can also list all files by typing \"print\". Type \"stop\" to stop the program.");
+            String input = scan.nextLine();
+	        if (input.equals("up")) {
+	        	System.out.println("What is the file path?");
+	        	String tempInput = scan.nextLine();
+	            resume.uploadResume(tempInput);
+	        } else if (input.equals("down")) {
+	        	System.out.println("Enter Object name to Download:");
+	        	String tempInput = scan.nextLine();
+	            resume.downloadResume(tempInput);
+	        } else if (input.equals("print")) {
+	        	URL url;
+				try {
+					url = new URL(httpPath);
+		        	HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		        	con.setRequestMethod("GET");
+		        	System.out.println(con.getOutputStream());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        } else if (input.equals("end")) {
+	        	System.out.println("Ending program...");
+	            System.exit(0);
+	        }
         }
-        switch(input.toLowerCase()) {
-            case "up": System.out.println("What is the file path?");
-            input = scan.nextLine();
-            resume.uploadResume(input);
-            break;
-        case "down":
-            System.out.println("What is the file path?");
-            input = scan.nextLine();
-            resume.downloadResume(input);
-            break;
-        case "print": //print
-        	URL url;
-			try {
-				url = new URL(httpPath);
-	        	HttpURLConnection con = (HttpURLConnection) url.openConnection();
-	        	con.setRequestMethod("GET");
-	        	System.out.println(con.getOutputStream());
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-            break;
-        case "end":
-            System.out.println("Ending program...");
-            System.exit(0);
-	        break;
-	    }
-	}
+    }
 }
 
